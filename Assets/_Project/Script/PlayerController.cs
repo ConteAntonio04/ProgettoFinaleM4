42ba3b75d4a85c53f28656e02edc04c192d4b280
ConteAntonio04 <antonioconte291@gmail.com>
2026-02-18 12:56:25 +0100

    PototypePlayer&Movement

---
+++ Assets/_Project/Script/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float jumpForce = 5f;
+    
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        rb.velocity = new Vector3(move.x * speed, rb.velocity.y, move.z * speed);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
---
+++ Assets/_Project/Script/VisualSystem.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualSystem : MonoBehaviour
+{
+    [Header("Visual Settings")]
+    [SerializeField]
+    private float mouseSensitivity = 1000f;
+    [SerializeField]
+    private Transform cameraPivot;
+
+    private float yRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Look();
+    }
+
+    void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+        yRotation -= mouseY;
+        yRotation = Mathf.Clamp(yRotation, -35f, 50f);
+
+        cameraPivot.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+    }
+}
